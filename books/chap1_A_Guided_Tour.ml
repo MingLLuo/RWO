2b9b3fb4e7f33f07c12bf3283e073271cc2e9fdd
MingLLuo <sh770240869@163.com>
2023-11-10 11:32:23 +0800

    add book chap1/2

---
+++ books/chap1_A_Guided_Tour.ml
@@ -0,0 +1,192 @@
+open Core
+
+(* function define *)
+let square x = x * x
+
+let printInt (x : int) = Caml.print_endline (string_of_int x)
+
+let printFloat (x : float) = Caml.print_endline (string_of_float x)
+
+let () = printInt (square 2)
+
+let () = printInt (square (square 2))
+
+(* other than int -> int *)
+let ratio x y = Float.of_int x /. Float.of_int y
+
+let () = printFloat (ratio 4 7)
+
+(* open module in let *)
+let ratio x y =
+  let open Float.O in
+  of_int x / of_int y
+
+let () = printFloat (ratio 4 7)
+
+let ratio x y =
+  let open Float.O in
+  of_int x / of_int y
+
+let () = printFloat (ratio 4 7)
+
+let ratio x y = Float.O.(of_int x / of_int y)
+
+let () = printFloat (ratio 4 7)
+
+(* type inference example *)
+let sum_if_true test first second =
+  (if test first then first else 0) + if test second then second else 0
+
+(* let sum_if_true (test : int -> bool) (x : int) (y : int) : int =
+   (if test x then x else 0)
+   + (if test y then y else 0);; *)
+
+(* generic types *)
+let first_if_true test x y = if test x then x else y
+
+(* val first_if_true : ('a -> bool) -> 'a -> 'a -> 'a = <fun> *)
+
+(* error example *)
+
+(* let add_potato x =
+   x + "potato";; *)
+(* run time error if y <= 0 *)
+let is_a_multiple x y = x % y = 0
+
+(* tuple patter matching *)
+let distance (x1, y1) (x2, y2) =
+  Float.sqrt (((x1 -. x2) **. 2.) +. ((y1 -. y2) **. 2.))
+
+(* diff operation in base and stdlib *)
+
+(* exp1: exponentiation *)
+(* stdlib *)
+let _ = 1. **. 2.
+
+let _ = 1. ** 2.
+
+(* Base *)
+let _ = Base.( ** ) 1 2
+
+let _ = Base.( **. ) 1. 2.
+
+(* comma for tuple, semicolon for list element *)
+let _ : (int * int * int) list = [(1, 2, 3)]
+
+let _ : int list = [1; 2; 3]
+
+(* String.rsplit2 split the string based on the rightmost period found in the string. *)
+let downcase_extension filename =
+  match String.rsplit2 filename ~on:'.' with
+  | None ->
+      filename
+  | Some (base, ext) ->
+      base ^ "." ^ String.lowercase ext
+
+let () =
+  Caml.print_endline
+    (List.to_string
+       ~f:(fun x -> x)
+       (List.map ~f:downcase_extension
+          ["Hello_World.TXT"; "Hello_World.txt"; "Hello_World"] ) )
+
+(* Array *)
+let printIntArray (a : int array) =
+  let lst = Array.to_list a in
+  match lst with
+  | [] ->
+      print_endline ""
+  | l ->
+      print_endline (List.to_string ~f:(fun x -> string_of_int x) l)
+
+let numbers = [|1; 2; 3; 4|]
+
+let () = printIntArray numbers
+
+let () = numbers.(2) <- 4
+
+let () = printIntArray numbers
+
+(* mutable record field *)
+type running_sum =
+  { mutable sum: float
+  ; mutable sum_sq: float (* sum of squares *)
+  ; mutable samples: int }
+
+let mean rsum = rsum.sum /. Float.of_int rsum.samples
+
+let stdev rsum =
+  Float.sqrt ((rsum.sum_sq /. Float.of_int rsum.samples) -. (mean rsum **. 2.))
+
+let create () = {sum= 0.; sum_sq= 0.; samples= 0}
+
+let update rsum x =
+  rsum.samples <- rsum.samples + 1 ;
+  rsum.sum <- rsum.sum +. x ;
+  rsum.sum_sq <- rsum.sum_sq +. (x *. x)
+
+let rsum = create ()
+
+let () = List.iter [1.; 3.; 2.; -7.; 4.; 5.] ~f:(fun x -> update rsum x)
+
+(* ref *)
+let x = {contents= 0} ;;
+
+x.contents <- x.contents + 1
+
+let x = ref 0 ;;
+
+x := !x + 1
+
+(* implement of ref *)
+type 'a ref = {mutable contents: 'a}
+
+let ref x = {contents= x}
+
+let ( ! ) r = r.contents
+
+let ( := ) r x = r.contents <- x
+
+(* for loop *)
+let permute array =
+  let length = Array.length array in
+  for i = 0 to length - 2 do
+    (* pick a j to swap with *)
+    let j = i + Random.int (length - i) in
+    (* Swap i and j *)
+    let tmp = array.(i) in
+    array.(i) <- array.(j) ;
+    array.(j) <- tmp
+  done
+
+let ar = Array.init 20 ~f:(fun i -> i) ;;
+
+permute ar
+
+let () = printIntArray ar
+
+(* while loop *)
+let find_first_negative_entry array =
+  let pos = ref 0 in
+  while
+    let pos_is_good = !pos < Array.length array in
+    let element_is_non_negative = array.(!pos) >= 0 in
+    pos_is_good && element_is_non_negative
+  do
+    pos := !pos + 1
+  done ;
+  if !pos = Array.length array then None else Some !pos
+;;
+
+find_first_negative_entry [|1; 2; 0; -3|]
+
+(* type ctrl + d *)
+let rec read_and_accumulate accum =
+  let line = In_channel.input_line In_channel.stdin in
+  match line with
+  | None ->
+      accum
+  | Some x ->
+      read_and_accumulate (accum +. Float.of_string x)
+
+let () = printf "Total: %F\n" (read_and_accumulate 0.)
---
+++ books/chap2_Variables_and_Functions.ml
@@ -0,0 +1,158 @@
+(* a variable with limited scope *)
+open Core
+
+let printInt (x : int) = Caml.print_endline (string_of_int x)
+
+let printFloat (x : float) = Caml.print_endline (string_of_float x)
+
+let languages = "OCaml,Perl,C++,C"
+
+let dashed_languages =
+  let language_list = String.split languages ~on:',' in
+  String.concat ~sep:"-" language_list
+
+let () = Caml.print_endline ""
+
+let () = Caml.print_endline dashed_languages
+
+(* use nested expression build larger computation *)
+let area_of_ring inner_radius outer_radius =
+  let pi = Float.pi in
+  let area_of_circle r = pi *. r *. r in
+  area_of_circle outer_radius -. area_of_circle inner_radius
+
+let () = Caml.print_endline (string_of_float (area_of_ring 1. 3.))
+
+(* pattern matching in both side *)
+let ints, strings = List.unzip [(1, "one"); (2, "two"); (3, "three")]
+
+let print_ints =
+  List.iter ints ~f:(fun x -> Caml.print_string (string_of_int x ^ " "))
+
+let () = Caml.print_endline ""
+
+let print_strings = List.iter strings ~f:(fun x -> Caml.print_string (x ^ " "))
+
+let () = Caml.print_endline ""
+
+let upcase_first_entry line =
+  match String.split ~on:',' line with
+  | [] ->
+      assert false (* String.split returns at least one element *)
+  | first :: rest ->
+      String.concat ~sep:"," (String.uppercase first :: rest)
+
+let () = Caml.print_endline (upcase_first_entry "hello,world")
+
+(* let and fun *)
+let () = printInt ((fun x -> x + 1) 7) [@ocamlformat "disable"]
+
+let () =
+  let x = 7 in
+  x + 1 |> printInt
+
+(* multiargument function *)
+let abs_diff = (
+  fun x -> (fun y -> abs (x - y))
+)  [@ocamlformat "disable"]
+
+let () = printInt (abs_diff 3 4)
+
+(* currying *)
+let abs_diff (x, y) = abs (x - y)
+
+let () = printInt (abs_diff (3, 4))
+
+(* recursive function *)
+let rec is_even x = if x = 0 then true else is_odd (x - 1)
+
+and is_odd x = if x = 0 then false else is_even (x - 1)
+
+let () = Caml.print_endline "Even or Odd" ;;
+
+List.map ~f:(fun x -> if is_even x then printInt x) [0; 1; 2; 3; 4; 5] ;;
+
+List.map ~f:(fun x -> if is_odd x then printInt x) [0; 1; 2; 3; 4; 5]
+
+(* prefix and infix *)
+let () = Int.max 3 4 |> printInt
+
+let () = 3 + 4 |> printInt
+
+let () = 3 + 4 |> printInt  [@ocamlformat "disable"]
+
+let () = List.map ~f:(( + ) 3) [1; 2; 3; 4] |> List.iter ~f:printInt
+
+(* difine or redifine operator *)
+let ( +! ) (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+let () =
+  (1, 2) +! (3, 4)
+  |> fun (x, y) -> Caml.print_endline (string_of_int x ^ "," ^ string_of_int y)
+
+(* will be a comment *)
+(* let (***) x y = (x **. y) **. y;; *)
+let ( *** ) x y = (x **. y) **. y
+
+(* reverse application operator *)
+(* left associative *)
+let ( |> ) x f = f x
+
+open Stdio
+
+let path = "/usr/bin:/usr/local/bin:/bin:/sbin"
+
+let () =
+  String.split ~on:':' path
+  |> List.dedup_and_sort ~compare:String.compare
+  |> List.iter ~f:print_endline
+;;
+
+let split_path = String.split ~on:':' path in
+let deduped_path = List.dedup_and_sort ~compare:String.compare split_path in
+List.iter ~f:print_endline deduped_path
+
+(* right associative *)
+(* https://v2.ocaml.org/api/Ocaml_operators.html *)
+let ( ^> ) x f = f x
+
+(* let () = String.split ~on:':' path ^> List.dedup_and_sort ~compare:String.compare ^> List.iter ~f:print_endline *)
+let () =
+  (String.split ~on:':' path ^> List.dedup_and_sort ~compare:String.compare)
+  ^> List.iter ~f:print_endline
+
+(* function *)
+let some_or_zero = function Some x -> x | None -> 0
+
+let some_or_zero num_opt = match num_opt with Some x -> x | None -> 0
+
+(* label arguments *)
+let ratio ~num ~denom = Float.of_int num /. Float.of_int denom
+
+let () = printFloat (ratio ~num:3 ~denom:4)
+
+(* argument label*)
+let apply_to_tuple_2 f (first, second) = f ~second ~first
+
+let divide ~first ~second = first / second
+
+(* error disordered argument *)
+(* apply_to_tuple_2 divide (4,2) *)
+let apply_to_tuple f (first, second) = f ~first ~second ;;
+
+apply_to_tuple divide (4, 2)
+
+(* optional argument *)
+let concat ?sep x y =
+  let sep = match sep with None -> "" | Some s -> s in
+  x ^ sep ^ y
+
+let () = Caml.print_endline (concat "hello" "world")
+
+let () = Caml.print_endline (concat ~sep:"," "hello" "world")
+
+let concat ?(sep = "") x y = x ^ sep ^ y
+
+let () = Caml.print_endline (concat ~sep:"," "hello" "world")
+
+let () = Caml.print_endline (concat ?sep:(Some ",") "hello" "world")
