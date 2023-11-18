c87d9710c885b0084ec541365a3c34126fcccd4a
MingLLuo <sh770240869@163.com>
2023-11-18 12:40:02 +0800

    fix

--- books/chap9_GADTs.ml
+++ books/chap9_GADTs.ml
@@ -15,7 +15,6 @@ type _ expr =
   | Plus : int expr * int expr -> int expr
   | If : bool expr * 'a expr * 'a expr -> 'a expr
 
-exception Ill_typed
 
 (* OCaml by default isn’t willing to instantiate ordinary type variables in different ways in the body of the same function *)
 (* We can fix that by adding a locally abstract type, which doesn’t have that restriction.   *)
